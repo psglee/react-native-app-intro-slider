309055c5629dca27f96c17d23e6a1557458b0487
Anh Tuan Le <anh.letuan.dev@gmail.com>
2019-08-20 13:45:55 +0700

    Update style

--- AppIntroSlider.js
+++ AppIntroSlider.js
@@ -209,7 +209,7 @@ const styles = StyleSheet.create({
     height: 16,
     margin: 16,
     flexDirection: 'row',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
   },
   dot: {
